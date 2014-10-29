d3efab507473914c5bd88df7c815bd9bf4e3fcfd
Nicholas Hamidjaja <nicholas@adskom.com>
2014-10-29 14:14:42 +0700

    Correct and update reset script

--- reset.sh
+++ reset.sh
@@ -1,6 +1,7 @@
-rails destroy controller ArticlesController
+rails destroy controller Articles
 rails destroy model Article
 rake db:drop
 cp config/routes.rb.blank config/routes.rb
+rm ./spec/routing/articles_routing_spec.rb
 
 exit 0
